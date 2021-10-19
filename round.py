d354029285772182e89fc9da82487936049e498a
Ubuntu <ubuntu@ip-172-31-70-134.ec2.internal>
2021-10-19 20:39:11 +0000

    added python code

---
+++ round.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Fletcher
+# Created on: Oct 2021
+# This program lets the user to input a numbers and rounds it
+
+import math
+
+
+def round_decimal(user_number, user_places):
+    # process
+    user_number[0] = (user_number[0] * (10 ** user_places)) + 0.5
+    user_number[0] = int(user_number[0])
+    user_number[0] = float(user_number[0])
+    user_number[0] = user_number[0] / (10 ** user_places)
+
+
+def main():
+    # calling functions and inputs
+    user_number = []
+
+    user_number_string = input("Enter the number you want to round: ")
+    places_user_string = input("Enter the decimal places you want to round by: ")
+
+    try:
+        user_number_int = float(user_number_string)
+        places_user_int = int(places_user_string)
+
+        user_number.append(user_number_int)
+
+        round_decimal(user_number, places_user_int)
+
+        print("")
+        print("The decimal rounded {0}".format(user_number[0]))
+        print("\nDone.")
+
+    except Exception:
+        print("")
+        print("These are invalid numbers.")
+
+
+if __name__ == "__main__":
+    main()
